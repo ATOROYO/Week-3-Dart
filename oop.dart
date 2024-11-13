7deecd36eb2b4c8fefc7963ea11daf1dcdd093bf
ATOROYO <davidatoroyosika@gmail.com>
2024-11-13 19:12:49 +0300

    Declaring a class in dart

---
+++ oop.dart
@@ -0,0 +1,14 @@
+// Declaring a class in dart
+class person {
+  // Properties of the class
+  String name;
+  int age;
+
+  // Constructor
+  person(this.name, this.age);
+
+  // Method to display person details
+  void displayInfo() {
+    print("Name: $name, Age: $age");
+  }
+}
